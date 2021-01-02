95cd3d1ea05b6de126a53c43867efbebbb21c409
Doktrjegih <52949561+Doktrjegih@users.noreply.github.com>
2021-01-02 18:54:45 +0500

    добавил вывод id стекера

--- telegram_bot_1.py
+++ telegram_bot_1.py
@@ -1,26 +1,39 @@
 import telebot
+import json
 
 bot = telebot.TeleBot('1489262010:AAENU2E27PWX-HswDGVWSGyLuACCOasmcTo')
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, False)
+keyboard1.row('Привет', 'Пока', 'Тест')
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
 
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.send_message(message.chat.id, 'Пока')
     elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'оно работает!':
+        bot.send_message(message.chat.id, 'Пфф пока')
+    elif message.text.lower() == 'тест':
         bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMsX-cUIHcnAWsC8x8tyavTEg3PwzwAAgoBAALuxKEKkj5OE68x5XgeBA')
+
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
-    print(message)
-    bot.send_message(message.chat.id, f'id этого стикера в конце сообщения {message}')
+    json_to_string = str(message)
+    a = json_to_string.replace("'", '"')
+    b = a.replace("<", '"')
+    c = b.replace(">", '"')
+    d = c.replace("False", "false")
+    e = d.replace("None", "null")
+    print(e)
+    a1 = json.loads(e)
+    A = a1['sticker']['file_id']
+    bot.send_message(message.chat.id, f'id этого стикера - {A}')
+
 
 bot.polling()
